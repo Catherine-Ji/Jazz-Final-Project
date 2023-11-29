b41bdd85df9b247b0247741ceab201434396b351
margotstern <margot.stern02@gmail.com>
2023-11-29 14:16:41 -0500

    added drums

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 pre-reqs:
 - install Mingus Library (https://bspaans.github.io/python-mingus/doc/wiki/tutorialSetup.html): `pip install mingus`
+- run pip install pygame
 - Download .SF2 soundfont file from https://archive.org/download/free-soundfonts-sf2-2019-04: Download the one named `FluidR3_GM2-2.SF2` and move it into the same directory as the python file.
 
 To run it:
--- TextToMusic.py
+++ TextToMusic.py
@@ -3,40 +3,10 @@ from mingus.midi import fluidsynth
 from mingus.containers import Note
 import time
 import random
+import numpy as np
+import pygame
 # import fluidsynth
 
-
-def main():
-    print("Hi there, I'm your music generator!")
-    text = input("please enter your text: ")
-    while not isEnglish(text):
-        text = input("Sorry, your string contains non-English characters, please try again:")
-    # fs = fluidsynth.Synth()
-    # fs.open("soundfont.SF2")
-    # fs.program_set(0, 6)
-    # fluidsynth.settings_add("synth.gain", 2.0)
-    fluidsynth.init("FluidR3_GM2-2.SF2")
-    fluidsynth.set_instrument(1, 6)
-    for letter in text:
-        if letter in dict.keys():
-            n = Note(dict[letter.lower()])
-        else:
-            n = Note(dict[random.choice(string.ascii_letters).lower()])
-        n.channel = 0
-        n.velocity = 50
-        fluidsynth.play_Note(n)
-        time.sleep(0.2)
-        fluidsynth.stop_Note(n)
-
-        # fs.program_change(0, 100, 127)
-        # Play the note and wait for 0.2 seconds
-        # fs.note_on(n)
-        # time.sleep(0.2)
-
-        # Stop the note
-        # fs.note_off(n)
-
-    
 dict = {
   "a": "A-3",
   "b": "B-3",
@@ -64,10 +34,64 @@ dict = {
   "x": "C-7",
   "y": "D-7",
   "z": "E-7",
-  " ": "C-2"
+  " ": "C-2",
 }
-    
+punctation = set(string.punctuation)
+punctation_dict = {
+    "!": 5,
+    "?": 2,
+    ".": 1,
+    "-": 2,
+    ",": 3,
+    ";": 4
+}
 
+def main():
+    print("""
+    Welcome to the Jazz Symphony Generator!
+
+        ♫    ♪                ♫     ♫
+        ♫        ♪         ♪           ♫
+          ♫           ♪             ♫
+             ♫                 ♫
+                 ♪     ♫
+    """)
+    while True:
+        text = input("Please enter your text to convert to jazz (type 'exit' to stop): ")
+        
+        if text.lower() == 'exit':
+            print("Thank you for playing :)")
+            break
+
+        while not isEnglish(text):
+            text = input("Sorry, your string contains non-English characters, please try again: ")
+
+        fluidsynth.init("FluidR3_GM2-2.SF2")
+        fluidsynth.set_instrument(1, 6)
+
+        for letter in text:
+            if letter in dict.keys():
+                n = Note(dict[letter.lower()])
+            elif letter in punctation_dict:
+                snare_sound_file = "./snare.wav"  # Replace with the path to your snare drum sound file
+                for _ in range(punctation_dict[letter]):
+                    play_drum_sound(snare_sound_file)
+                    n = Note(dict[random.choice(string.ascii_letters).lower()])
+                    n.channel = 0
+                    n.velocity = 50
+                    fluidsynth.play_Note(n)
+                    time.sleep(0.2)
+                    fluidsynth.stop_Note(n)
+                continue
+            else:
+                n = Note(dict[random.choice(string.ascii_letters).lower()])
+                
+            n.channel = 0
+            n.velocity = 50
+            fluidsynth.play_Note(n)
+            time.sleep(0.2)
+            fluidsynth.stop_Note(n)
+    
 def isEnglish(text):
     try:
         text.encode(encoding='utf-8').decode('ascii')
@@ -75,6 +99,11 @@ def isEnglish(text):
         return False
     else:
         return True
+def play_drum_sound(sound_file):
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound(sound_file)
+    sound.play()
+    time.sleep(1)
 
 if __name__ == "__main__":
     main()
